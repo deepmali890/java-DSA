225c110a5ebcd8178b95f5f567c71c18dcfcd35a
Dileep Mali <dileepmali@amityonline.com>
2026-05-25 15:16:01 +0530

    intrduction function

---
+++ src/functions/Introduction.java
@@ -0,0 +1,20 @@
+package functions;
+
+public class Introduction {
+
+    // Function Creation
+    // This function prints "Hello World"
+    public static void printHello() {
+
+        System.out.println("Hello World");
+
+    }
+
+    public static void main(String[] args) {
+
+        // Function Call
+        // Calling the function to execute it
+        printHello();
+
+    }
+}
